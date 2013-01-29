cf4665bc279300f54a87f4df528268ea7e860099
Luca Antiga <luca.antiga@orobix.com>
2013-01-30 00:58:26 +0100

    Factored exodelay code out of macro.

--- test/exoref/delay_test.clj
+++ test/exoref/delay_test.clj
@@ -7,12 +7,9 @@
   (testing "Testing exodelay in a local environmnet"
     (let [d1 (exodelay "foo:delay")
           d2 (exodelay "foo:delay" (reduce + (range 1000)))]
-      (prn "A")
       (is (and (= false (realized? d1)) (= false (realized? d2))))
-      (prn "B")
       (future (deref d1))
       (. Thread (sleep 5000))
-      (prn "C")
       (is (= true (realized? d1)))
       (is (= true (realized? d2)))
       (is (= @d1 (reduce + (range 1000))))
